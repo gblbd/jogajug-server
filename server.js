95af7e10d210b85d2b7706cac795b1d824bca807
gblbd <gbl.devs@gmail.com>
2023-03-11 13:13:09 +0600

    fix swagger

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument  =require("./swagger.json");
+const swaggerJSDoc = require("swagger-jsdoc");
 require("dotenv").config();
 
 const app = express();
@@ -23,6 +26,8 @@ app.use(fileUpload());
 app.use(bodyParser.json({ limit: "50mb" }));
 //app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors()); // allows all origins
+//swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // routes attached with server
 app.use("/api", require("./routes/auth"));
