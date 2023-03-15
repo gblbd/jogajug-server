02bfca8f610b23676ff0747e15e184cec4074c2a
imranhosain6554 <imran122.office@gmail.com>
2023-03-15 16:45:30 +0600

    fix bugs in weather

--- controllers/weatherReport.js
+++ controllers/weatherReport.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 const express = require('express');
 
-const app = express();
-
 exports.readWeatherDetails = async (req, res) => {
     console.log('hello');
     try {
@@ -12,18 +10,22 @@ exports.readWeatherDetails = async (req, res) => {
         console.log(currentTime);
         const API_KEY = 'ce7eca7e7a61b5b5c411d3d7927fa1b7';
         const CITY_NAME = 'Dhaka';
+
         const response = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`,
+            `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`
         );
 
         const weather = response.data.weather[0].description;
         const temperature = response.data.main.temp;
-        console.log(`Current weather in ${CITY_NAME}: ${weather}, Temperature: ${temperature}K`);
-        const ConvertToF = 1.8(temperature - 273) + 32;
+        const humidityData = response.data.main.humidity;
+        console.log(
+            `Current weather in ${CITY_NAME}: ${weather}, Temperature: ${temperature}K , Humidity: ${humidityData}`,
+        );
+
         console.log(response);
         // just ajker weather report dhekasse
-        res.json({ weather, temperatureInF: ConvertToF });
+        return res.json({ weather, temperature, humidityData });
     } catch (err) {
-        return res.status(400).json(err);
+        return res.json(err);
     }
 };
