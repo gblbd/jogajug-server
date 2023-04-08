c56629b206e6dae88604be04e2e64385c7e10713
imranhosain6554 <imran122.office@gmail.com>
2023-04-08 14:22:40 +0600

    fix weather integration

--- controllers/weatherReport.js
+++ controllers/weatherReport.js
@@ -5,23 +5,32 @@ exports.readWeatherDetails = async (req, res) => {
     console.log('hello');
     try {
         // today current times weather report
+        const cityName = req.query.city || 'Dhaka';
+        console.log(cityName);
         const currentDate = new Date();
         const currentTime = currentDate.getTime() / 1000; // convert to UNIX timestamp
-        console.log(currentTime);
+
         const API_KEY = 'ce7eca7e7a61b5b5c411d3d7927fa1b7';
-        const CITY_NAME = 'Dhaka';
+        // const CITY_NAME = 'Dhaka';
 
         const response = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME}&appid=${API_KEY}`
+            `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
         );
 
         const weather = response.data.weather[0].description;
         const temperature = response.data.main.temp;
         const humidityData = response.data.main.humidity;
+        const feelsWEATHER = response.data.main.feels_like;
 
-        console.log(response);
+        // console.log(response);
         // just ajker weather report dhekasse.Temp in kelvin, not Farenheight
-        return res.json({ weather, temperature, humidityData });
+        return res.json({
+            weather,
+            temperature,
+            humidityData,
+            currentDate,
+            feelsWEATHER,
+        });
     } catch (err) {
         return res.json(err);
     }
